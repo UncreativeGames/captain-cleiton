d460afc13110c754b35eefbfc4b8b34ff96056ae
igor <igraphmagollo@gmail.com>
2018-04-24 21:28:27 -0300

    Tentativa de concertar espaçamentos

--- Obstacle.cpp
+++ Obstacle.cpp
@@ -25,22 +25,22 @@ Obstacle::Obstacle(const sf::Texture& texture, const sf::IntRect& rectangle, int
 	m_textureRect = rect;
 
 	m_vertices[0].position = sf::Vector2f(0.f, 0.f);
-    m_vertices[1].position = sf::Vector2f(0.f, static_cast<float>(rect.height));
-    m_vertices[2].position = sf::Vector2f(static_cast<float>(rect.width), static_cast<float>(rect.height));
-    m_vertices[3].position = sf::Vector2f(static_cast<float>(rect.width), 0.f);
+	m_vertices[1].position = sf::Vector2f(0.f, static_cast<float>(rect.height));
+	m_vertices[2].position = sf::Vector2f(static_cast<float>(rect.width), static_cast<float>(rect.height));
+	m_vertices[3].position = sf::Vector2f(static_cast<float>(rect.width), 0.f);
 
-    float left = static_cast<float>(rect.left) + 0.0001f;
-    float right = left + static_cast<float>(rect.width);
-    float top = static_cast<float>(rect.top);
-    float bottom = top + static_cast<float>(rect.height);
+	float left = static_cast<float>(rect.left) + 0.0001f;
+	float right = left + static_cast<float>(rect.width);
+	float top = static_cast<float>(rect.top);
+	float bottom = top + static_cast<float>(rect.height);
 
-    m_vertices[0].texCoords = sf::Vector2f(left, top);
-    m_vertices[1].texCoords = sf::Vector2f(left, bottom);
-    m_vertices[2].texCoords = sf::Vector2f(right, bottom);
-    m_vertices[3].texCoords = sf::Vector2f(right, top);
+	m_vertices[0].texCoords = sf::Vector2f(left, top);
+	m_vertices[1].texCoords = sf::Vector2f(left, bottom);
+	m_vertices[2].texCoords = sf::Vector2f(right, bottom);
+	m_vertices[3].texCoords = sf::Vector2f(right, top);
 
-    this->setPosition(x,y);
-    this->setRaio(0);
+	this->setPosition(x,y);
+	this->setRaio(0);
 }
 
 void Obstacle::setTexture(const sf::Texture& textura){
@@ -49,19 +49,19 @@ void Obstacle::setTexture(const sf::Texture& textura){
 	m_textureRect = rect;
 
 	m_vertices[0].position = sf::Vector2f(0.f, 0.f);
-    m_vertices[1].position = sf::Vector2f(0.f, static_cast<float>(rect.height));
-    m_vertices[2].position = sf::Vector2f(static_cast<float>(rect.width), static_cast<float>(rect.height));
-    m_vertices[3].position = sf::Vector2f(static_cast<float>(rect.width), 0.f);
+	m_vertices[1].position = sf::Vector2f(0.f, static_cast<float>(rect.height));
+	m_vertices[2].position = sf::Vector2f(static_cast<float>(rect.width), static_cast<float>(rect.height));
+	m_vertices[3].position = sf::Vector2f(static_cast<float>(rect.width), 0.f);
 
-    float left = static_cast<float>(rect.left) + 0.0001f;
-    float right = left + static_cast<float>(rect.width);
-    float top = static_cast<float>(rect.top);
-    float bottom = top + static_cast<float>(rect.height);
+	float left = static_cast<float>(rect.left) + 0.0001f;
+	float right = left + static_cast<float>(rect.width);
+	float top = static_cast<float>(rect.top);
+	float bottom = top + static_cast<float>(rect.height);
 
-    m_vertices[0].texCoords = sf::Vector2f(left, top);
-    m_vertices[1].texCoords = sf::Vector2f(left, bottom);
-    m_vertices[2].texCoords = sf::Vector2f(right, bottom);
-    m_vertices[3].texCoords = sf::Vector2f(right, top);
+	m_vertices[0].texCoords = sf::Vector2f(left, top);
+	m_vertices[1].texCoords = sf::Vector2f(left, bottom);
+	m_vertices[2].texCoords = sf::Vector2f(right, bottom);
+	m_vertices[3].texCoords = sf::Vector2f(right, top);
 }
 
 const sf::Texture* Obstacle::getTexture() const
@@ -73,19 +73,19 @@ void Obstacle::setTextureRect(const sf::IntRect& rect){
 	m_textureRect = rect;
 
 	m_vertices[0].position = sf::Vector2f(0.f, 0.f);
-    m_vertices[1].position = sf::Vector2f(0.f, static_cast<float>(rect.height));
-    m_vertices[2].position = sf::Vector2f(static_cast<float>(rect.width), static_cast<float>(rect.height));
-    m_vertices[3].position = sf::Vector2f(static_cast<float>(rect.width), 0.f);
+	m_vertices[1].position = sf::Vector2f(0.f, static_cast<float>(rect.height));
+	m_vertices[2].position = sf::Vector2f(static_cast<float>(rect.width), static_cast<float>(rect.height));
+	m_vertices[3].position = sf::Vector2f(static_cast<float>(rect.width), 0.f);
 
-    float left = static_cast<float>(rect.left) + 0.0001f;
-    float right = left + static_cast<float>(rect.width);
-    float top = static_cast<float>(rect.top);
-    float bottom = top + static_cast<float>(rect.height);
+	float left = static_cast<float>(rect.left) + 0.0001f;
+	float right = left + static_cast<float>(rect.width);
+	float top = static_cast<float>(rect.top);
+	float bottom = top + static_cast<float>(rect.height);
 
-    m_vertices[0].texCoords = sf::Vector2f(left, top);
-    m_vertices[1].texCoords = sf::Vector2f(left, bottom);
-    m_vertices[2].texCoords = sf::Vector2f(right, bottom);
-    m_vertices[3].texCoords = sf::Vector2f(right, top);
+	m_vertices[0].texCoords = sf::Vector2f(left, top);
+	m_vertices[1].texCoords = sf::Vector2f(left, bottom);
+	m_vertices[2].texCoords = sf::Vector2f(right, bottom);
+	m_vertices[3].texCoords = sf::Vector2f(right, top);
 }
 
 const sf::IntRect& Obstacle::getTextureRect() const
