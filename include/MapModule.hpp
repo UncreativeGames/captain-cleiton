9e3a4ec831fd3f6d1c500ad88b015410832fb07e
Gabriel Olivato <olivatooo@gmail.com>
2018-05-03 22:50:06 -0300

    Removida comparação desnecessária

---
+++ include/MapModule.hpp
@@ -0,0 +1,16 @@
+//
+// Created by olivato on 03/05/18.
+//
+
+#ifndef CAPTAIN_CLEITON_MAPMODULE_H
+#define CAPTAIN_CLEITON_MAPMODULE_H
+
+
+
+class MapModule {
+
+};
+
+
+
+#endif //CAPTAIN_CLEITON_MAPMODULE_H
